e3bad93b83cd64efd0d56e155b131e017f3b8ef2
Taku Nakajima <tnaka@dc4.so-net.ne.jp>
2014-02-24 14:15:26 +0900

    first commit

---
+++ src/test/java/sample/MainTest.java
@@ -0,0 +1,14 @@
+
+package sample;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+public class MainTest {
+    
+    @Test
+    public void testAdd() {
+        assertThat(Main.add(10, 5), is(15));
+        assertThat(Main.add(10, 5), is(16));
+    }
+}
+
